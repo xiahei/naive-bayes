b38d78de9ded2d6928cc232c60667e523aecc8a5
xiahei <dyxin24@sina.com>
2017-09-12 15:08:19 +0800

    Update seg_test.py

--- seg_test.py
+++ seg_test.py
@@ -6,6 +6,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import re
 import os
+from glob import glob
 
 if len(sys.argv) != 3:
     print 'input parameter error'
@@ -15,31 +16,31 @@ if len(sys.argv) != 3:
 label = sys.argv[1]
 classname = sys.argv[2]
 
-count = 1
-filename = './training_set/'+label+'_'+classname+'/'+label+'_'+classname+'_texts/text_'+str(count)+'.txt'
-outputname = 'seg_train/'+label+'_'+classname+'/seg_text'+str(count)+'.txt'
+inputpath = './training_set/'+label+'_'+classname+'/'+label+'_'+classname+'_texts'
+#filename = 'text_'+str(count)+'.txt'
+outputpath = 'seg_train/'+label+'_'+classname
+#outputname = '/seg_text'+str(count)+'.txt'
 pattern = re.compile('[v]*n.*')
 
-while(count < 14110):
-    if(os.path.isfile(filename)):
-        fr = open(filename,'r')
-        output = open(outputname,'w')
-        for line in fr.readlines():
-            #sentence = unicode(line.strip())
-            sentence = line.strip()
-            words = list(posseg.cut(sentence))
-            for word in words:
-                #print word.word,word.flag        
-                if pattern.match(word.flag):
-                    #print word.word,word.flag
-                    output.write(word.word + '\t')
-        fr.close()
-        output.close()
-        print count
-        count += 1
-        filename = './training_set/'+label+'_'+classname+'/'+label+'_'+classname+'_texts/text_'+str(count)+'.txt'
-        outputname = 'seg_train/'+label+'_'+classname+'/seg_text'+str(count)+'.txt'
-    else:
-        count += 1
-        filename = './training_set/'+label+'_'+classname+'/'+label+'_'+classname+'_texts/text_'+str(count)+'.txt'
-        outputname = 'seg_train/'+label+'_'+classname+'/seg_text'+str(count)+'.txt'
+if not os.path.exists(outputpath):
+    os.makedirs(outputpath)
+
+file_list = glob(inputpath+'/text_*.txt')
+for inputname in file_list:
+    print inputname
+    fr = open(inputname,'r')
+    outputname = inputname.replace(inputpath,outputpath)
+    print outputname
+    output = open(outputname,'w')
+    for line in fr.readlines():
+        #sentence = unicode(line.strip())
+        sentence = line.strip()
+        words = list(posseg.cut(sentence))
+        #words = posseg.lcut(sentence) #可直接返回list
+        for word in words:
+            #print word.word,word.flag        
+            if pattern.match(word.flag): #word.flag:词性
+                #print word.word,word.flag
+                output.write(word.word + '\t')
+    fr.close()
+    output.close()
